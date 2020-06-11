e69f810acddde9bfc31b08fe7d15fa9971d3c8d6
Oliver Davies <oliver@oliverdavies.uk>
2020-06-11 23:49:15 +0100

    Refactor, extract a method

--- src/Argv.php
+++ src/Argv.php
@@ -62,16 +62,23 @@ final class Argv
 
                     $nextArg = $argsArray[$i + 1];
 
-                    if (!$this->isArgumentOrOption($nextArg)) {
-                      return [$currentArg => $nextArg];
-                    }
-
-                    if ($this->isArgumentOrOption($currentArg)) {
-                        return [$currentArg => true];
-                    }
-
-                    return [$nextArg => true];
+                    return $this->addNextValue($currentArg, $nextArg);
                 }
             );
+    }
+
+    private function addNextValue(
+        string $currentArg,
+        string $nextArg
+    ): array {
+        if (!$this->isArgumentOrOption($nextArg)) {
+            return [$currentArg => $nextArg];
+        }
+
+        if ($this->isArgumentOrOption($currentArg)) {
+            return [$currentArg => true];
+        }
+
+        return [$nextArg => true];
     }
 }
